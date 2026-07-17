6e69f0bf6019006008190b1ef163521a34a18a4d
Adam Burdett <burdettadam@gmail.com>
2026-07-17 17:25:03 -0600

    remove deprecated unreachable authenticator code

--- lib/main.dart
+++
@@ -1,3 +0,0 @@
-import 'mains/main_marty.dart' as app;
-
-void main() => app.main();
--- lib/widgets/widget_keys.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/foundation.dart';
-
-// const two_step_dialog_title = ValueKey('two_step_dialog_title');
-const twoStepDialogContent = ValueKey('two_step_dialog_content');
-// const two_step_dialog_button = ValueKey('two_step_dialog_button');
